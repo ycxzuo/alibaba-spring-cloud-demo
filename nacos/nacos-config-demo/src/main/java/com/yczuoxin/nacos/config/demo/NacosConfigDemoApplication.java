416515045110234a1a9f9d54516827b816bb943d
zuoxin <343720929@qq.com>
2019-05-22 16:39:11 +0800

    Nacos项目搭建

---
+++ nacos/nacos-config-demo/src/main/java/com/yczuoxin/nacos/config/demo/NacosConfigDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yczuoxin.nacos.config.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfigDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigDemoApplication.class, args);
+    }
+
+}
